c957614511daff21e0d1ef9acb4a262c35b3eeb9
lee-orr <leeorr@gmail.com>
2022-09-06 01:26:00 +0000

    add stub for file list component

---
+++ frontend-ts/src/components/FileList.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export default function FileList({}: {}): ReactElement {
+    return (
+        <div>
+            HERE
+        </div>
+    )
+}
--- frontend-ts/src/pages/Session.tsx
+++ frontend-ts/src/pages/Session.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from "react";
 import { useParams } from "react-router-dom";
 import Split from "react-split";
+import FileList from "components/FileList"
 
 export default function SessionPage({isHost}: {isHost: boolean}): ReactElement {
     const { name, project, code } = useParams<{ name: string | undefined, project: string | undefined, code: string | undefined }>()
@@ -8,7 +9,9 @@ export default function SessionPage({isHost}: {isHost: boolean}): ReactElement {
         <div className="h-screen flex flex-col items-stretch justify-start">
             <div className="bg-slate-900 p-2 flex flex-row justify-center">{isHost ? 'Hosting' : 'Joining'}, {name} @ { project ?? code ?? "No code or project"}</div>
             <div className="flex flex-row flex-grow">
-                <div className="bg-slate-800 border-r-2 border-r-slate-900 p-2 flex flex-col justify-start w-56">Here</div>
+                <div className="bg-slate-800 border-r-2 border-r-slate-900 p-2 flex flex-col justify-start w-56">
+                    <FileList/>
+                </div>
                 <Split className="split flex flex-row flex-grow">
                     <div className="p-2">Test</div>
                     <div className="p-2">Me</div>
