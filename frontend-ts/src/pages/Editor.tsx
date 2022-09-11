a939ea87194f8f19c76be1456206a2ff623c4c12
lee-orr <leeorr@gmail.com>
2022-09-11 20:09:33 +0000

    rendering fountain in editor

--- frontend-ts/src/pages/Editor.tsx
+++ frontend-ts/src/pages/Editor.tsx
@@ -2,7 +2,7 @@ import Button from 'components/Button'
 import type { ReactElement } from 'react'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { createEditor, Node } from 'slate'
-import type { RenderElementProps } from 'slate-react'
+import type { RenderElementProps, RenderLeafProps } from 'slate-react'
 import { Editable, withReact, Slate } from 'slate-react'
 import { withYjs, YjsEditor } from '@slate-yjs/core'
 import type { SyncedFile } from 'utils/SyncedFile'
@@ -59,11 +59,62 @@ export default function Editor({
 
 	const renderElement = useCallback(
 		/* eslint-disable react/jsx-props-no-spreading */
-		({ attributes, children, element }: RenderElementProps): JSX.Element => (
-			<p className={`slate-element slate-${element.type}`} {...attributes}>{children}</p>
-		),
+		({ attributes, children, element }: RenderElementProps): JSX.Element => {
+			let className = `slate-element slate-${element.type} `;
+			let currentText = element.children.map((t): string => t.text).join('');
+			if (currentText.startsWith('>') && currentText.endsWith('<')) {
+				className += 'text-center'
+			}
+			switch (element.type) {
+				case 'title_element':
+					className += 'text-sm text-gray-400'
+					break;
+				case 'character':
+					className += 'text-center text-lg'
+					break;
+				case 'dialogue':
+					className += 'text-center'
+					break;
+				case 'lyrics':
+					className += 'text-left pl-[20%]'
+					break;
+				case 'scene_header':
+					className += 'text-left text-2xl'
+					break;
+				case 'transition':
+					className += 'text-right text-lg'
+					break;
+				case 'page_break':
+					className += 'text-xs text-gray-400 border-b-2 border-gray-900'
+					break;
+				case 'boneyard':
+					className += `text-xs bg-slate-900 pl-2 text-gray-400`
+					break;
+				default:
+					break;
+			}
+			return (
+			<p className={className} {...attributes}>{children}</p>
+		)
+			},
 		[]
 	)
+
+	const renderLeaf = useCallback(({ attributes, children, leaf }: RenderLeafProps) => {
+		if (leaf.text === '&nbsp;') {
+			return <span {...attributes} className='text-xs text-gray-700'>{children}</span>
+		}
+		if (leaf.text.startsWith('(') && leaf.text.endsWith(')')) {
+			return <span {...attributes} className='text-sm text-gray-400'>{children}</span>
+		}
+		return (
+		  <span
+			{...attributes}
+		  >
+			{children}
+		  </span>
+		)
+	  }, [])
 
 	return (
 		<div className='flex flex-col items-stretch justify-start'>
@@ -80,7 +131,7 @@ export default function Editor({
 					style={preview ? { position: 'fixed', bottom: '-200vh' } : {}}
 				>
 					<Slate editor={editor} value={value}>
-						<Editable renderElement={renderElement} />
+						<Editable renderElement={renderElement} renderLeaf={renderLeaf} />
 					</Slate>
 				</div>
 				{preview ? (
--- frontend-ts/src/utils/SlateFountain.ts
+++ frontend-ts/src/utils/SlateFountain.ts
@@ -1,7 +1,32 @@
-import { Editor, BaseEditor, Path, Node, Element, Transforms } from 'slate'
+import { Editor, BaseEditor, Path, Node, Element, Transforms, Text } from 'slate'
 import type { ReactEditor } from 'slate-react'
 interface TitleElement {
 	type: 'title_element'
+	children: CustomText[]
+}
+
+interface SceneHeader {
+	type: 'scene_header'
+	children: CustomText[]
+}
+
+interface Transition {
+	type: 'transition'
+	children: CustomText[]
+}
+
+interface CharacterElement {
+	type: 'character',
+	children: CustomText[]
+}
+
+interface DialogueElement {
+	type: 'dialogue',
+	children: CustomText[]
+}
+
+interface LyricElement {
+	type: 'lyrics',
 	children: CustomText[]
 }
 
@@ -15,6 +40,18 @@ interface Empty {
 	children: []
 }
 
+interface PageBreak {
+	type: 'page_break',
+	children: CustomText[]
+}
+
+interface BoneYard {
+	type: 'boneyard',
+	containsStart: boolean,
+	containsEnd: boolean,
+	children: CustomText[]
+}
+
 interface CustomText {
 	text: string
 }
@@ -22,7 +59,7 @@ interface CustomText {
 declare module 'slate' {
 	interface CustomTypes {
 		Editor: BaseEditor & ReactEditor
-		Element: {edit?: boolean} & (RawText | TitleElement | Empty)
+		Element: { edit?: boolean } & (RawText | BoneYard | PageBreak | Transition | SceneHeader | TitleElement | Empty | CharacterElement | DialogueElement | LyricElement)
 		Text: CustomText
 	}
 }
@@ -41,20 +78,33 @@ const withFountain = (editor: Editor): Editor => {
 			let next_element: Element | false = false;
 			try {
 				next_element = Node.get(editor, next_path) as Element
-			} catch {}
+			} catch { }
 			let currentText = node.children.map((t): string => t.text).join('');
 			let edited = false
 
-			if (node.type !== "empty" && node.children.length === 1 && node.children[0].text === '') {
+			if (node.type !== "empty" && currentText.length === 0 && (!previous_element || previous_element.type !== 'boneyard')) {
 				Transforms.setNodes(editor, { type: "empty", children: [] }, { at: path })
 				edited = true
-			} 
-
+			}
 			if (!edited)
-				edited = ProcessTitle(node, editor, path, currentText, previous_element)
+				edited = ProcessArbitraryControls(node, editor, path, currentText, previous_element)
 
+			if (currentText.length > 0 && node.type !== 'boneyard') {
+				if (node.type === 'empty') {
+					Transforms.setNodes(editor, { type: "raw" }, { at: path })
+					edited = true
+				}
+				if (!edited)
+					edited = ProcessTitle(node, editor, path, currentText, previous_element)
+
+				if (!edited)
+					edited = ProcessHeadingsAndTransitions(node, editor, path, currentText, previous_element)
+
+				if (!edited)
+					edited = ProcessDialogue(node, editor, path, currentText, previous_element)
+			}
 			if (edited) {
-				if (next_element) Transforms.setNodes(editor, { edit: !next_element.edit}, { at: next_path})
+				if (next_element) Transforms.setNodes(editor, { edit: !next_element.edit }, { at: next_path })
 				return
 			}
 		}
@@ -81,3 +131,102 @@ function ProcessTitle(node: Element, editor: Editor, path: Path, currentText: st
 	return false
 }
 
+function ProcessDialogue(node: Element, editor: Editor, path: Path, currentText: string, previous_element: false | Element): boolean {
+	if (currentText.length === 0 || node.type === 'transition') return false;
+	if ((!previous_element || previous_element.type === 'empty') && (currentText.startsWith('@') || currentText === currentText.toUpperCase()) && node.type !== 'character') {
+		Transforms.setNodes(editor, { type: 'character', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if ((!previous_element || previous_element.type === 'empty') && currentText !== currentText.toUpperCase() && !currentText.startsWith('@') && node.type === 'character') {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+
+	if (currentText.startsWith('~') && previous_element && (previous_element.type === 'character' || previous_element.type === 'dialogue' || previous_element.type === 'lyrics') && node.type !== 'lyrics') {
+		Transforms.setNodes(editor, { type: 'lyrics', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (node.type === 'lyrics' && ((!previous_element || (previous_element.type !== 'character' && previous_element.type !== 'dialogue' && previous_element.type !== 'lyrics')) || !currentText.startsWith('~'))) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+
+	if (previous_element && (previous_element.type === 'character' || previous_element.type === 'dialogue' || previous_element.type === 'lyrics') && node.type !== 'dialogue' && node.type !== 'lyrics') {
+		Transforms.setNodes(editor, { type: 'dialogue', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (node.type === 'dialogue' && (!previous_element || (previous_element.type !== 'character' && previous_element.type !== 'dialogue' && previous_element.type !== 'lyrics'))) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	return false
+}
+
+const sceneHeaders = ['.',
+	'INT',
+	'EXT',
+	'EST',
+	'INT./EXT',
+	'INT/EXT',
+	'I/E']
+
+function ProcessHeadingsAndTransitions(node: Element, editor: Editor, path: Path, currentText: string, previous_element: false | Element): boolean {
+	if (currentText.length === 0) return false;
+	const uppercase = currentText.toUpperCase()
+	if (node.type !== 'scene_header' && sceneHeaders.map((h) => uppercase.startsWith(h)).includes(true)) {
+		Transforms.setNodes(editor, { type: 'scene_header', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (node.type === 'scene_header' && !sceneHeaders.map((h) => uppercase.startsWith(h)).includes(true)) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+
+	if (node.type !== 'transition' && (currentText.startsWith('>') && !currentText.endsWith('<') || currentText.endsWith('TO:') && currentText === uppercase)) {
+		Transforms.setNodes(editor, { type: 'transition', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (node.type === 'transition' && ((!currentText.startsWith('>') || currentText.endsWith('<')) && (!currentText.endsWith('TO:') || currentText !== uppercase))) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	return false
+}
+
+function ProcessArbitraryControls(node: Element, editor: Editor, path: Path, currentText: string, previous_element: false | Element): boolean {
+	const pageBreak = /^===+$/.test(currentText);
+	if (pageBreak && node.type !== 'page_break') {
+		Transforms.setNodes(editor, { type: 'page_break', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (!pageBreak &&  node.type === 'page_break') {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (currentText.startsWith('/*') && currentText.endsWith('*/') && node.type !== 'boneyard') {
+		Transforms.setNodes(editor, { type: 'boneyard', containsStart: true, containsEnd: true}, {at: path})
+		return true
+	}
+	if (currentText.startsWith('/*') && node.type !== 'boneyard') {
+		Transforms.setNodes(editor, { type: 'boneyard', containsStart: true, containsEnd: false}, {at: path})
+		return true
+	}
+	if (currentText.endsWith('*/') && node.type !== 'boneyard' && previous_element && previous_element.type === 'boneyard') {
+		Transforms.setNodes(editor, { type: 'boneyard', containsEnd: true, containsStart: false}, {at: path})
+		return true
+	}
+	if (previous_element && previous_element.type === 'boneyard' && !previous_element.containsEnd && node.type !== 'boneyard') {
+		Transforms.setNodes(editor, { type: 'boneyard', containsStart: false, containsEnd: false}, {at: path})
+		return true
+	}
+	if (node.type === 'boneyard' && node.containsStart && !currentText.startsWith('/*')) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+	if (node.type === 'boneyard' && !node.containsStart && (!previous_element || previous_element.type !== 'boneyard')) {
+		Transforms.setNodes(editor, { type: 'raw', children: [{ text: currentText }] }, { at: path })
+		return true
+	}
+
+	return false
+}
