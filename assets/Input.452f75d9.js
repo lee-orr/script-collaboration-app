745094232e4eb54ab8666eb4c8cdf458ec466ea9
lee-orr <lee-orr@users.noreply.github.com>
2022-10-28 18:05:09 +0000

    Deploying to gh-pages from @ lee-orr/script-collaboration-app@5ef67bd2d59f75bb3c3c7ae81c19db5873e9c8bd 🚀

--- assets/Input.452f75d9.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./index.e4e9eba6.js";function p({value:r,input:t}){return a("input",{className:"border-b-2 border-b-slate-400 bg-transparent p-1",value:r,onInput:n=>t(n.currentTarget.value)})}export{p as I};
